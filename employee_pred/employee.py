78fdf8a20c4fcd54bc2a751ea3f83aebdeec855f
OwitiH <100336311+OwitiH@users.noreply.github.com>
2023-06-06 15:31:21 +0300

    Update employee.py

--- employee_pred/employee.py
+++ employee_pred/employee.py
@@ -1,36 +1,4 @@
-df={'Performance': {0: 78,1: 46,2: 54,3: 49,4: 40,5: 44,6: 41,7: 40,8: 43,9: 42, 10: 45,11: 66,12: 59,13: 64,14: 47,15: 56, 16: 55,
- 17: 51,18: 60,19: 77,20: 39,21: 59,22: 80,23: 61,24: 64,25: 47,26: 56,27: 55,28: 51,29: 60,30: 77,31: 39,32: 59,33: 80},
- 'Age': {0: 45,1: 21,2: 33,3: 34,4: 22,5: 24,6: 26,7: 27,8: 28,9: 23,10: 29,11: 38,12: 38,13: 39,14: 67,15: 68,16: 75,17: 73,18: 71,19: 43,20: 21,21: 70,22: 49,23: 37,
-  24: 28,25: 23,26: 29,27: 38,28: 38,29: 39,30: 67,31: 68,32: 75,33: 73},
- 'Sickoff': {0: 14,1: 1,2: 4,3: 2,4: 0,5: 0,6: 2,7: 1,8: 0,9: 0,10: 1,11: 1,12: 1,13: 1,14: 20,15: 21,16: 24,17: 19, 18: 18, 19: 2, 20: 1,
-  21: 22,22: 1,23: 1,24: 2,25: 2,26: 1,27:3,28: 4,29: 5,30: 6,
-  31: 5,32: 20,33: 22},'Gender': {0: 'M', 1: 'M',2: 'F',3: 'M', 4: 'F',5: 'F',6: 'M',7: 'M',8: 'M',9: 'F', 10: 'M',
-  11: 'M',12: 'F',13: 'M',14: 'F',15: 'F',16: 'F',17: 'M',18: 'M',19: 'M',20: 'F',21: 'F',22: 'M',23: 'M',24: 'M',25: 'F', 26: 'M',
-  27: 'F',28: 'M',29: 'F',30: 'M',31: 'F',32: 'F', 33: 'M'},'Citizen': {0: 'local', 1: 'local',2: 'exper',3: 'exper',4: 'local',
-  5: 'local',6: 'exper',7: 'local',8: 'local',9: 'local',10: 'local',11: 'local',12: 'local',13: 'local',14: 'exper',15: 'exper',16: 'exper',17: 'exper',18: 'local',19: 'local',20: 'local',
-  21: 'exper',22: 'local',23: 'local',24: 'local',25: 'local',26: 'local',27: 'exper',28: 'exper',29: 'exper', 30: 'local',31: 'local',32: 'exper', 33: 'local'},'Povert_index': {0: 'poor',1: 'poor',2: 'poor',3: 'well_off',4: 'well_off',5: 'well_off',6: 'poor',
-  7: 'poor',8: 'well_off',9: 'poor', 10: 'well_off',11: 'poor',12: 'well_off',13: 'poor',14: 'poor',15: 'well_off',
-  16: 'well_off',17: 'poor',18: 'poor',19: 'poor',20: 'well_off', 21: 'well_off',22: 'poor',23: 'well_off',24: 'well_off',
-  25: 'well_off',26: 'poor',27: 'poor',28: 'well_off',29: 'poor',
-  30: 'well_off',31: 'well_off',32: 'well_off',33: 'poor'},
- 'Sales': {0: 'good ',1: 'average',2: 'below average',3: 'good ',4: 'good ',5: 'good ',6: 'average',7: 'good ',8: 'good ',9: 'below average',10: 'average',11: 'average', 12: 'below average',13: 'average',14: 'good ',15: 'average',16: 'average',17: 'below average',18: 'average',19: 'below average',20: 'average',21: 'average',22: 'below average',23: 'below average',24: 'good ',25: 'good ',26: 'good ',27: 'good ',28: 'average',29: 'below average',30: 'good ',31: 'below average',32: 'good ',33: 'good '}}
-df0={'Performance': {0: 78,1: 46,2: 54,3: 49,4: 40,5: 44,6: 41,7: 40,8: 43,9: 42, 10: 45,11: 66,12: 59,13: 64,14: 47,15: 56, 16: 55,
- 17: 51,18: 60,19: 77,20: 39,21: 59,22: 80,23: 61,24: 64,25: 47,26: 56,27: 55,28: 51,29: 60,30: 77,31: 39,32: 59,33: 80},
- 'Age': {0: 45,1: 21,2: 33,3: 34,4: 22,5: 24,6: 26,7: 27,8: 28,9: 23,10: 29,11: 38,12: 38,13: 39,14: 67,15: 68,16: 75,17: 73,18: 71,19: 43,20: 21,21: 70,22: 49,23: 37,
-  24: 28,25: 23,26: 29,27: 38,28: 38,29: 39,30: 67,31: 68,32: 75,33: 73},
- 'Sickoff': {0: 14,1: 1,2: 4,3: 2,4: 0,5: 0,6: 2,7: 1,8: 0,9: 0,10: 1,11: 1,12: 1,13: 1,14: 20,15: 21,16: 24,17: 19, 18: 18, 19: 2, 20: 1,
-  21: 22,22: 1,23: 1,24: 2,25: 2,26: 1,27:3,28: 4,29: 5,30: 6,
-  31: 5,32: 20,33: 22},'Gender': {0: 'M', 1: 'M',2: 'F',3: 'M', 4: 'F',5: 'F',6: 'M',7: 'M',8: 'M',9: 'F', 10: 'M',
-  11: 'M',12: 'F',13: 'M',14: 'F',15: 'F',16: 'F',17: 'M',18: 'M',19: 'M',20: 'F',21: 'F',22: 'M',23: 'M',24: 'M',25: 'F', 26: 'M',
-  27: 'F',28: 'M',29: 'F',30: 'M',31: 'F',32: 'F', 33: 'M'},'Citizen': {0: 'local', 1: 'local',2: 'exper',3: 'exper',4: 'local',
-  5: 'local',6: 'exper',7: 'local',8: 'local',9: 'local',10: 'local',11: 'local',12: 'local',13: 'local',14: 'exper',15: 'exper',16: 'exper',17: 'exper',18: 'local',19: 'local',20: 'local',
-  21: 'exper',22: 'local',23: 'local',24: 'local',25: 'local',26: 'local',27: 'exper',28: 'exper',29: 'exper', 30: 'local',31: 'local',32: 'exper', 33: 'local'},'Povert_index': {0: 'poor',1: 'poor',2: 'poor',3: 'well_off',4: 'well_off',5: 'well_off',6: 'poor',
-  7: 'poor',8: 'well_off',9: 'poor', 10: 'well_off',11: 'poor',12: 'well_off',13: 'poor',14: 'poor',15: 'well_off',
-  16: 'well_off',17: 'poor',18: 'poor',19: 'poor',20: 'well_off', 21: 'well_off',22: 'poor',23: 'well_off',24: 'well_off',
-  25: 'well_off',26: 'poor',27: 'poor',28: 'well_off',29: 'poor',
-  30: 'well_off',31: 'well_off',32: 'well_off',33: 'poor'},
- 'Sales': {0: 'good ',1: 'average',2: 'below average',3: 'good ',4: 'good ',5: 'good ',6: 'average',7: 'good ',8: 'good ',9: 'below average',10: 'average',11: 'average', 12: 'below average',13: 'average',14: 'good ',15: 'average',16: 'average',17: 'below average',18: 'average',19: 'below average',20: 'average',21: 'average',22: 'below average',23: 'below average',24: 'good ',25: 'good ',26: 'good ',27: 'good ',28: 'average',29: 'below average',30: 'good ',31: 'below average',32: 'good ',33: 'good '}}
-
+#importing recquired libraries
 from dash import Dash,html,dcc,dash_table,callback,State,Input,Output
 import dash_bootstrap_components as dbc
 import pandas as pd
@@ -40,8 +8,9 @@ import plotly.express as px
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import accuracy_score
-df0=pd.DataFrame(df0)
-df=pd.DataFrame(df)
+
+df=read.csv("data.csv")
+df0=read.csv("data.csv)
 
 leb=LabelEncoder()
 df["Gender"]=leb.fit_transform(df["Gender"])
