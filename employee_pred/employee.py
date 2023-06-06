effeee2dd0fdca067c69f7790fb178848a0ab4b0
OwitiH <100336311+OwitiH@users.noreply.github.com>
2023-06-06 15:19:57 +0300

    Add files via upload

---
+++ employee_pred/employee.py
@@ -0,0 +1,147 @@
+df={'Performance': {0: 78,1: 46,2: 54,3: 49,4: 40,5: 44,6: 41,7: 40,8: 43,9: 42, 10: 45,11: 66,12: 59,13: 64,14: 47,15: 56, 16: 55,
+ 17: 51,18: 60,19: 77,20: 39,21: 59,22: 80,23: 61,24: 64,25: 47,26: 56,27: 55,28: 51,29: 60,30: 77,31: 39,32: 59,33: 80},
+ 'Age': {0: 45,1: 21,2: 33,3: 34,4: 22,5: 24,6: 26,7: 27,8: 28,9: 23,10: 29,11: 38,12: 38,13: 39,14: 67,15: 68,16: 75,17: 73,18: 71,19: 43,20: 21,21: 70,22: 49,23: 37,
+  24: 28,25: 23,26: 29,27: 38,28: 38,29: 39,30: 67,31: 68,32: 75,33: 73},
+ 'Sickoff': {0: 14,1: 1,2: 4,3: 2,4: 0,5: 0,6: 2,7: 1,8: 0,9: 0,10: 1,11: 1,12: 1,13: 1,14: 20,15: 21,16: 24,17: 19, 18: 18, 19: 2, 20: 1,
+  21: 22,22: 1,23: 1,24: 2,25: 2,26: 1,27:3,28: 4,29: 5,30: 6,
+  31: 5,32: 20,33: 22},'Gender': {0: 'M', 1: 'M',2: 'F',3: 'M', 4: 'F',5: 'F',6: 'M',7: 'M',8: 'M',9: 'F', 10: 'M',
+  11: 'M',12: 'F',13: 'M',14: 'F',15: 'F',16: 'F',17: 'M',18: 'M',19: 'M',20: 'F',21: 'F',22: 'M',23: 'M',24: 'M',25: 'F', 26: 'M',
+  27: 'F',28: 'M',29: 'F',30: 'M',31: 'F',32: 'F', 33: 'M'},'Citizen': {0: 'local', 1: 'local',2: 'exper',3: 'exper',4: 'local',
+  5: 'local',6: 'exper',7: 'local',8: 'local',9: 'local',10: 'local',11: 'local',12: 'local',13: 'local',14: 'exper',15: 'exper',16: 'exper',17: 'exper',18: 'local',19: 'local',20: 'local',
+  21: 'exper',22: 'local',23: 'local',24: 'local',25: 'local',26: 'local',27: 'exper',28: 'exper',29: 'exper', 30: 'local',31: 'local',32: 'exper', 33: 'local'},'Povert_index': {0: 'poor',1: 'poor',2: 'poor',3: 'well_off',4: 'well_off',5: 'well_off',6: 'poor',
+  7: 'poor',8: 'well_off',9: 'poor', 10: 'well_off',11: 'poor',12: 'well_off',13: 'poor',14: 'poor',15: 'well_off',
+  16: 'well_off',17: 'poor',18: 'poor',19: 'poor',20: 'well_off', 21: 'well_off',22: 'poor',23: 'well_off',24: 'well_off',
+  25: 'well_off',26: 'poor',27: 'poor',28: 'well_off',29: 'poor',
+  30: 'well_off',31: 'well_off',32: 'well_off',33: 'poor'},
+ 'Sales': {0: 'good ',1: 'average',2: 'below average',3: 'good ',4: 'good ',5: 'good ',6: 'average',7: 'good ',8: 'good ',9: 'below average',10: 'average',11: 'average', 12: 'below average',13: 'average',14: 'good ',15: 'average',16: 'average',17: 'below average',18: 'average',19: 'below average',20: 'average',21: 'average',22: 'below average',23: 'below average',24: 'good ',25: 'good ',26: 'good ',27: 'good ',28: 'average',29: 'below average',30: 'good ',31: 'below average',32: 'good ',33: 'good '}}
+df0={'Performance': {0: 78,1: 46,2: 54,3: 49,4: 40,5: 44,6: 41,7: 40,8: 43,9: 42, 10: 45,11: 66,12: 59,13: 64,14: 47,15: 56, 16: 55,
+ 17: 51,18: 60,19: 77,20: 39,21: 59,22: 80,23: 61,24: 64,25: 47,26: 56,27: 55,28: 51,29: 60,30: 77,31: 39,32: 59,33: 80},
+ 'Age': {0: 45,1: 21,2: 33,3: 34,4: 22,5: 24,6: 26,7: 27,8: 28,9: 23,10: 29,11: 38,12: 38,13: 39,14: 67,15: 68,16: 75,17: 73,18: 71,19: 43,20: 21,21: 70,22: 49,23: 37,
+  24: 28,25: 23,26: 29,27: 38,28: 38,29: 39,30: 67,31: 68,32: 75,33: 73},
+ 'Sickoff': {0: 14,1: 1,2: 4,3: 2,4: 0,5: 0,6: 2,7: 1,8: 0,9: 0,10: 1,11: 1,12: 1,13: 1,14: 20,15: 21,16: 24,17: 19, 18: 18, 19: 2, 20: 1,
+  21: 22,22: 1,23: 1,24: 2,25: 2,26: 1,27:3,28: 4,29: 5,30: 6,
+  31: 5,32: 20,33: 22},'Gender': {0: 'M', 1: 'M',2: 'F',3: 'M', 4: 'F',5: 'F',6: 'M',7: 'M',8: 'M',9: 'F', 10: 'M',
+  11: 'M',12: 'F',13: 'M',14: 'F',15: 'F',16: 'F',17: 'M',18: 'M',19: 'M',20: 'F',21: 'F',22: 'M',23: 'M',24: 'M',25: 'F', 26: 'M',
+  27: 'F',28: 'M',29: 'F',30: 'M',31: 'F',32: 'F', 33: 'M'},'Citizen': {0: 'local', 1: 'local',2: 'exper',3: 'exper',4: 'local',
+  5: 'local',6: 'exper',7: 'local',8: 'local',9: 'local',10: 'local',11: 'local',12: 'local',13: 'local',14: 'exper',15: 'exper',16: 'exper',17: 'exper',18: 'local',19: 'local',20: 'local',
+  21: 'exper',22: 'local',23: 'local',24: 'local',25: 'local',26: 'local',27: 'exper',28: 'exper',29: 'exper', 30: 'local',31: 'local',32: 'exper', 33: 'local'},'Povert_index': {0: 'poor',1: 'poor',2: 'poor',3: 'well_off',4: 'well_off',5: 'well_off',6: 'poor',
+  7: 'poor',8: 'well_off',9: 'poor', 10: 'well_off',11: 'poor',12: 'well_off',13: 'poor',14: 'poor',15: 'well_off',
+  16: 'well_off',17: 'poor',18: 'poor',19: 'poor',20: 'well_off', 21: 'well_off',22: 'poor',23: 'well_off',24: 'well_off',
+  25: 'well_off',26: 'poor',27: 'poor',28: 'well_off',29: 'poor',
+  30: 'well_off',31: 'well_off',32: 'well_off',33: 'poor'},
+ 'Sales': {0: 'good ',1: 'average',2: 'below average',3: 'good ',4: 'good ',5: 'good ',6: 'average',7: 'good ',8: 'good ',9: 'below average',10: 'average',11: 'average', 12: 'below average',13: 'average',14: 'good ',15: 'average',16: 'average',17: 'below average',18: 'average',19: 'below average',20: 'average',21: 'average',22: 'below average',23: 'below average',24: 'good ',25: 'good ',26: 'good ',27: 'good ',28: 'average',29: 'below average',30: 'good ',31: 'below average',32: 'good ',33: 'good '}}
+
+from dash import Dash,html,dcc,dash_table,callback,State,Input,Output
+import dash_bootstrap_components as dbc
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score
+df0=pd.DataFrame(df0)
+df=pd.DataFrame(df)
+
+leb=LabelEncoder()
+df["Gender"]=leb.fit_transform(df["Gender"])
+df["Citizen"]=leb.fit_transform(df["Citizen"])
+df["Povert_index"]=leb.fit_transform(df["Povert_index"])
+df["Sales"]=leb.fit_transform(df["Sales"])
+y=df["Performance"]
+df2=df0.head()
+p=df[["Performance","Age","Citizen"]].corr()
+q=df[["Performance","Povert_index","Gender"]].corr()
+p
+model=LinearRegression()
+x=df[["Age","Citizen","Sales","Povert_index","Gender","Sickoff"]]
+fi= model.fit(x,y)
+fi.intercept_
+prediction=fi.predict([[23,0,1,1,0,2]])
+
+title=dbc.Row([dbc.Card([html.H2(["PREDICTION OF EMPLOYEE % PERFORMANCE "],className="card-header",style={"color":"rgb(255,255,255)"})],className="bg-dark",)],style={"paddingLeft":"2%","width":"100vw"})
+expla=dbc.Row([dbc.Card([dbc.CardHeader([html.H5("Purpose",style={"font-size":"20px","color":"rgb(192,192,192)"})]),dbc.CardBody([html.P(["The aim of this project is to help companies who want to employ  persons on a permanent basis should consider employing them on contract terms first. Then use this app to evaluate their performance.Those with exceptional performance should be considered.The Data was collected using google forms. "],style={"font-size":"20px","color":"rgb(192,192,192)"}),html.P(
+    "Performance being our depedent variable(performance being predicted based on sales,gender,age,citizenship,number of sickoff days and poverty index)",style={"font-size":"20px","color":"rgb(192,192,192)"})])],className="bg-dark")])
+corr_tab1=dbc.Card([dbc.CardHeader([html.H5("correlation table",style={"font-size":"20px","color":"rgb(192,192,192)"})]),dbc.CardBody([html.Div((dash_table.DataTable(p.to_dict('records'),style_data={"background-color":"rgb(0,0,0)","color":"rgb(0,0,255)"}))),dbc.CardFooter("@aronconsultancy")])],color="dark",style={"width":"50vw"})
+corr_tab2=dbc.Card([dbc.CardHeader([html.H5("correlation table",style={"font-size":"20px","color":"rgb(192,192,192)"})]),dbc.CardBody([html.Div((dash_table.DataTable(q.to_dict('records'),style_data={"background-color":"rgb(0,0,0)","color":"rgb(0,0,255)"}))),dbc.CardFooter("@aronconsultancy")])],color="dark",style={"width":"50vw"})
+data_sample_tab=dbc.Card([dbc.CardHeader([html.H5("Sample data table",style={"font-size":"20px","color":"rgb(192,192,192)"})]),dbc.CardFooter(""),
+                          dbc.CardBody([dash_table.DataTable(df2.to_dict('records'),style_data={"background-color":"rgb(0,0,0)","color":"rgb(0,0,255)"})])],color="dark",style={"width":"42vw"})
+
+#plots
+fig=px.bar(df0,x=["Gender","Citizen","Sales","Povert_index"],y="Performance",color='Gender')
+fig2=px.scatter_3d(df0,x="Sickoff",z="Age",y="Performance",color="Gender")
+fig3=px.imshow(df)
+fig4=px.bar(df0,x="Povert_index",y="Performance",color='Povert_index')
+fig5=px.scatter(df0,x="Age",y="Performance",color="Sickoff")
+scatter_age_Per=bar_for_povert_fig=dbc.Card([dbc.CardHeader([html.H5("Scatter plot for performance, age,sickoff",style={"font-size":"20px","color":"rgb(192,192,192)"},)]),dbc.CardBody([dcc.Graph(figure=fig5)])],color="dark")
+bar_for_povert_fig=dbc.Card([dbc.CardHeader([html.H5("performance povert bar graph",style={"font-size":"40px%","color":"rgb(255,255,255)"})]),dbc.CardBody([dcc.Graph(figure=fig4)])],color="dark")
+bar_for_sales_fig=dbc.Card([dbc.CardHeader([html.H5("Heatmap of all the variables",style={"font-size":"40px%","color":"rgb(255,255,255)"})]),dbc.CardBody([dcc.Graph(figure=fig3)])],color="dark")
+bar_for_citizen_fig=dbc.Card([dbc.CardHeader([html.H5("3D cluster plot of age,sickoff and performance ",style={"font-size":"40px%","color":"rgb(255,255,255)"})]),dbc.CardBody([dcc.Graph(figure=fig2)])],color="dark")
+bar_for_gender_fig=dbc.Card([dbc.CardHeader([html.H5("Count plot of input values",style={"font-size":"40px%","color":"rgb(255,255,255)"})]),dbc.CardBody([dcc.Graph(figure=fig)])],color="dark")
+pre_card=dbc.Card([html.Div([html.H6("Age",style={"marginLeft":"30%","font-size":"18px%","color":"rgb(255,255,255)","paddingLeft":"%","width":""}),
+                   dcc.Input(id="a",type="number",min=14,max=90,style={"marginLeft":"25%","border":"2%auto","border-radius":"20%","paddingLeft":"25%","width":"15vw"}),
+                   html.H6("Citizen[local input 1,foreign input 0]",style={"marginLeft":"15%","font-size":"18px","color":"rgb(255,255,255)","paddingLeft":"","width":""}),
+                   dcc.Input(id="c",type="number",min=0,max=1,style={"marginLeft":"25%","border":"2%auto","border-radius":"20%","paddingLeft":"25%","width":"15vw"}),
+                   html.H6("sales made during contract[good input 2,average input 0,below average input 1]",style={"marginLeft":"15%","font-size":"18px","color":"rgb(255,255,255)","paddingLeft":"","width":""}),
+                   dcc.Input(id="s",type="number",min=0,max=2,style={"marginLeft":"25%","border":"2%auto","border-radius":"20%","paddingLeft":"25%","width":"15vw"}),
+                   html.H6("povert index[poor input  0,welloff input 1] ",style={"marginLeft":"15%","font-size":"18px","color":"rgb(255,255,255)","paddingLeft":"","width":""}),
+                   dcc.Input(id="p",type="number",min=0,max=1,style={"marginLeft":"25%","border":"2%auto","border-radius":"20%","paddingLeft":"25%","width":"15vw"})
+                   ,html.H6("Gender[male input 1,female input 0",style={"marginLeft":"15%","font-size":"18px","color":"rgb(255,255,255)","paddingLeft":"","width":""}),
+                   dcc.Input(id="g",type="number",min=0,max=1,style={"marginLeft":"25%","border":"2%auto","border-radius":"20%","paddingLeft":"25%","width":"15vw"}),
+                   html.H6("sick off days",style={"marginLeft":"30%","font-size":"18px","color":"rgb(255,255,255)","paddingLeft":"","width":""}),
+                   dcc.Input(id="si",type="number",min=0,max=5000,style={"marginLeft":"25%","border":"2%auto","border-radius":"20%","paddingLeft":"25%","width":"15vw"}),
+
+                   dbc.Button(id="submit",children="PREDICT",style={"marginLeft":"70%","border":"2%auto","border-radius":"20%","background-color":"rgb(220,0,0)","width":"10vw"}),
+                   ])],color="dark")
+anime=html.Div([],)
+outpre=dbc.Card([html.Div(id="predout") ,html.P("OUTPUT WILL COMEOUT HERE")],style={"background-color":"rgb(192,192,192)"})
+ro_for_g_c=dbc.Row([dbc.Col([bar_for_gender_fig]),dbc.Col([bar_for_citizen_fig])],className="two columns g-0",style={"paddingLeft":"2%","width":"100vw"})
+ro_for_s_p = dbc.Row([dbc.Col([bar_for_sales_fig]), dbc.Col([bar_for_povert_fig])], className="two columns g-0",style={"paddingLeft":"2%","width":"100vw"})
+ro_for_scatter_p = dbc.Row([dbc.Col([scatter_age_Per],width=9), ],style={"paddingLeft":"2%","width":"100vw"})
+#card_tab3=dbc.Card([dbc.CardHeader("PEDICT")],color="primary")
+
+
+app=Dash(__name__,external_stylesheets=[dbc.themes.ZEPHYR])
+app.title="salesemployeeperformance"
+app.layout=html.Div(children=[dbc.Navbar([dbc.DropdownMenu([dbc.DropdownMenuItem([dbc.NavLink("About me",href="https://www.blogger.com/profile/07293138198715749524")]),
+    dbc.DropdownMenuItem([dbc.NavLink("visit my github",href="https://github.com/")]),
+   dbc.DropdownMenuItem([dbc.NavLink("Linkdn",href="https://www.linkedin.com/in/wanga-harron-464371161/")]),
+  dbc.DropdownMenuItem([dbc.NavLink("visit my website",href="https://stats-guru.blogspot.com/?m=1")])],label="MENUBAR",style={"bakground-color":"rgb(192,192,192)"}),html.H4("Ar0NConsuLTAncy LTD",style={"color":"rgb(220,20,60)","font-size":"30px","paddingLeft":"50%"})],style={"paddingLeft":"2%","height":"8vh","width":"100vw",},className="bg-dark"),
+  dcc.Tabs(id="tabs_id",children=[dcc.Tab(value="T1",label="HOME",className="bg-primary",style={"marginLeft":"2%","border":"2%auto","border-radius":"20%","background-color":"rgb(220,0,0)","width":"10vw"}),
+  dcc.Tab(value="T2",label=" CLICK FOR CORRELATION",className="bg-danger",style={"marginLeft":"2%","border":"2%auto","border-radius":"20%","background-color":"rgb(220,0,0)","width":"10vw"}),
+  dcc.Tab(value="T3",label="CLICK FOR PREDICT",className="bg-warning",style={"marginLeft":"2%","border":"2%auto","border-radius":"20%","background-color":"rgb(220,0,0)","width":"10vw"})],style={"width":"100vw"}),html.Div(id="click_tab")])
+@app.callback(
+    Output("predout","children"),
+    [Input("submit","n_clicks")],
+    [State("a","value"),
+     State("c","value"),
+     State("s","value"),
+     State("p","value"),
+     State("g","value"),
+     State("si","value")])
+def update_output(n_clicks,a,c,s,p,g,si):
+    if n_clicks==0:
+        return " "
+    else:
+        ma=fi.predict([[a,c,s,p,g,si]])
+        return html.H5("THE PREDICTED % PERFORMANCE IS:{}".format(ma),style={"color":"rgb(255,0,0)","font-size":"40px"})
+
+
+@app.callback(Output("click_tab","children"),
+              Input("tabs_id","value")
+             # State("tabs_id","value"))
+             )
+def update_content(tab):
+    if tab=="T1":
+        return  html.Div([dbc.Row([title],style={"paddingLeft":"2%","width":"100vw"}),dbc.Row([dbc.Col([expla]),dbc.Col([data_sample_tab])],className="two columns g-0",style={"paddingLeft":"2%","width":"100vw"}),ro_for_g_c,ro_for_s_p],style={"background-color":"rgb(255,215,0)"})
+    if tab=="T2":
+        return html.Div([ro_for_scatter_p,dbc.Row([corr_tab1,corr_tab2])],style={"background-color":"rgb(255,215,0)"})
+    if tab=="T3":
+        return html.Div([dbc.Row([dbc.Col([pre_card],width=5,style={"margin-left":"10px"}),dbc.Col([outpre],width=3)],className="two columns g-0",style={"paddingLeft":"2%","width":"100vw"})],className="bg-dark")
+
+    else :
+        return html.Div([dbc.Row([title],style={"paddingLeft":"2%","width":"100vw"}),dbc.Row([dbc.Col([expla]),dbc.Col([data_sample_tab])],className="two columns g-0",style={"paddingLeft":"2%","width":"100vw"}),ro_for_g_c,ro_for_s_p],style={"background-color":"rgb(255,215,0)"})
+
+
+if __name__=="__main__":
+    app.run_server()
